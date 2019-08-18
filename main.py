2d882ace7958d98b3f351a68518d76f40fe28682
Marlon <marlonfuenteslp@gmail.com>
2019-08-17 18:55:42 -0600

    added train

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
-url = 'https://raw.githubusercontent.com/fuentesmarlon/CLUSTERING-PCA-APRIORI/master/test.csv'
+url = 'https://raw.githubusercontent.com/fuentesmarlon/CLUSTERING-PCA-APRIORI/master/train.csv'
 df = pd.read_csv(url)
 
 print(list(df.columns.values))
-print(len(list(df.columns.values)))
+newDf =df[[""]]
