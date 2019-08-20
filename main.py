ed218dbd8aee1c11d4f8c49654fb6cdb52213ea0
Marlon <marlonfuenteslp@gmail.com>
2019-08-19 22:05:39 -0600

    added ipython notebook

--- main.py
+++
@@ -1,9 +0,0 @@
-import os 
-import pandas as pd 
-import matplotlib.pyplot as plt 
-
-url = 'https://raw.githubusercontent.com/fuentesmarlon/CLUSTERING-PCA-APRIORI/master/train.csv'
-df = pd.read_csv(url)
-
-print(list(df.columns.values))
-newDf =df[[""]]
